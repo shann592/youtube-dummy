62b6cd43dcee5db020f05354b3800db50fb5be27
Shantanu Singh <shannformal323@gmail.com>
2022-09-16 13:23:24 +0530

    app.js modified

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
+const searchBar = document.querySelector('.search-bar');
+
+searchBar.addEventListener('keypress', e => {
+    if(e.key === 'Enter') {
+        searchAction();
+    }
+})
+
 document.querySelector('.search-icon').addEventListener('click', e => {
-    let searchVal = document.querySelector('.search-bar').value;
+    searchAction();
+});
+
+const searchAction = () => {
+    const searchVal = searchBar.value.trim();
     let url = `https://www.youtube.com/results?search_query=${searchVal.trim()}`;
     window.open(url, '_blank');
-});
+}
